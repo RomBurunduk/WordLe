91fff024b35be041cbbd3027416121fd36787661
onlytrall <fylhtq.2003@mail.ru>
2023-01-26 22:42:56 +0300

    changed strategy

--- StupidButFastWordle/Player.cpp
+++ StupidButFastWordle/Player.cpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Interactor.cpp"
-#include "Init.cpp"
 #include <iostream>
 
 using namespace std;
--- StupidButFastWordle/main.cpp
+++ StupidButFastWordle/main.cpp
@@ -6,7 +6,6 @@
 #include <algorithm>
 #include <cmath>
 #include <ctime>
-#include "Word.cpp"
 #include "Init.cpp"
 #include "Interactor.cpp"
 #include "Player.cpp"
@@ -20,9 +19,11 @@ int calc(Word s, vector<int> &rate) {
     int score = 1;
     int cnt = 0;
 
-    vector<int> used(32, 1);
+    vector<double> used(32, 1);
     for(int i = 0;i < WORD_LEN;i++) {
-        score += sqrt(rate[s.GetSymb(i)]) * used[s.GetSymb(i)];
+        score += (rate[s.GetSymb(i)]) * used[s.GetSymb(i)];
+        // cout << sqrt(rate[s.GetSymb(i)]) * used[s.GetSymb(i)] << ' ';
+        // cout << endl;
         used[s.GetSymb(i)] *= 0.5;
     }
 
@@ -32,7 +33,7 @@ int calc(Word s, vector<int> &rate) {
 int check(Word s) {
     int score = 1;
     int cnt = 0;
-    for(int j = 0;j < 33;j++) {
+    for(int j = 0;j < 32;j++) {
         int i = j;
         for(auto pos : must[i]) {
             if(s.GetSymb(pos) != i)
@@ -40,13 +41,11 @@ int check(Word s) {
         }
     }
 
-    int curr = 0;
     for(int i = 0;i < WORD_LEN;i++) {
         int c = s.GetSymb(i);
-        if(state[c][curr] == 0)
+        if(state[c][i] == 0)
             return 0;
-        curr++;
-        score += 5 - state[c].count();
+        score += (WORD_LEN - state[c].count() + 3) * 9;
     }
 
     return score;
@@ -68,16 +67,28 @@ void solve(Word word) {
 
     int bestPos = 1737;
     int bestScore = 0;
+
+    vector<bool> goodWord(all.size(), true);
     for(int q = 0;q < 5;q++) {
-        vector<int> rate(33);
+
+        vector<int> rate(32);
         bestScore = 0;
-        for(int i = 0;i < 33;i++) {
-            rate[i] = (state[i].count()) * letterCost[i];
+        fill(letterCost.begin(), letterCost.end(), 0);
+
+        for(int i = 0;i < all.size();i++) {
+            if(!goodWord[i]) continue;
+            for(int j = 0;j < WORD_LEN;j++) {
+                letterCost[all[i].GetSymb(j)]++;
+            }
+        }
+
+        for(int i = 0;i < 32;i++) {
+            rate[i] = (state[i].count()) * sqrt(letterCost[i]) * must[i].empty();
         }
 
         for(int i = 0;i < all.size();i++) {
-            //if(check(all[i]) && i) continue;
-            int newVal = calc(all[i], rate);
+            if(!goodWord[i]) continue;
+            int newVal = check(all[i]) + calc(all[i], rate);
             if(bestScore < newVal) {
                 bestPos = i;
                 bestScore = newVal;
@@ -85,6 +96,8 @@ void solve(Word word) {
         }
         
         p.ask(inter, all[bestPos]);
+        for(int i = 0;i < all.size();i++)
+            goodWord[i] = check(all[i]);
     }
 
     int summ = 0;
